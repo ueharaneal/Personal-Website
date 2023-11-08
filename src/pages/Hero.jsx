2ec1629078dc102cd6e5eec2df313f93e58f35e9
Neal Uehara <ueharaneal@gmail.com>
2023-11-07 16:33:48 -0800

    fixed font

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = ({ setSelectedPage }) => {
     "bg-gradient-to-r from-purple-300 to-[#7B68EE] text-black text-sm rounded-md w-[200px] font-medium mx-auto py-2 my-8 w-[120px] md:w-[200px] md:text-xl hover:-translate-y-1 hover:scale-110 transition-transform duration-200 hover:text-[#E1D9D1]";
   return (
     <section id="home" className="text-[#EBE4D1]">
-      <div className="flex flex-col h-screen w-full justify-center text-center mx-auto max-w-[800px] mt-[96px] p-3 ">
+      <div className="flex flex-col h-screen w-full justify-center text-center mx-auto max-w-[800px] mt-[96px] p-3">
         <p className="text-[#7B68EE] md:text-lg font-bold p-2">
           Your search for the right Software Developer ends here.
         </p>
@@ -18,30 +18,37 @@ const Hero = ({ setSelectedPage }) => {
           Neal Uehara
         </h1>
 
-        <div className="flex flex-row text-center justify-center py-3 md:py-5">
-          <p className="py-2 px-3 font-bold text-xl md:text-4xl md:px-4">a </p>
+        <div className="flex flex-row text-center justify-center pt-3 md:pt-5">
+          <p
+            style={{ alignSelf: "flex-end" }}
+            className="pb-3 px-3 font-bold text-xl md:text-4xl md:px-4"
+          >
+            a
+          </p>
           <Typed
-            className="text-[#7B68EE]  font-bold text-3xl md:text-5xl"
+            className="flex items-center pb-3 text-[#7B68EE] font-bold text-3xl md:text-6xl"
             strings={["Front-end", "Back-end", "Full-stack"]}
             typeSpeed={120}
             backSpeed={140}
             loop
           />
-          <p className=" py-2 px-4 font-bold text-xl md:text-4xl">Developer</p>
+          <p
+            style={{ alignSelf: "flex-end" }}
+            className="pb-3 px-2 font-bold text-xl md:text-4xl md:px-4"
+          >
+            Developer
+          </p>
         </div>
         <p className="text-gray-500 font-bold text-lg md:text-2xl mx-14">
-          Full-stack developer dedicated to problem solving, thriving in dynamic
-          learning.
+          Full-stack developer dedicated to problem solving, thriving in dynamic learning.
         </p>
         <div className="flex justify-center">
           <button className={`${buttonClassname} mr-2`}>My Resume</button>
-          <button className={`${buttonClassname} ml-2`}>
-            See Website Code
-          </button>
+          <button className={`${buttonClassname} ml-2`}>See Website Code</button>
         </div>
       </div>
     </section>
   );
-};
+}; 
 
 export default Hero;
