357991c6c71ad2680ac3dec544875e95f508b5e7
Neal Uehara <ueharaneal@gmail.com>
2023-12-02 19:59:46 -0800

    contact

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
+
 import { useForm } from 'react-hook-form'
+
+import squares from '../assets/squares.mp4'
 
 
 function ContactForm() {
@@ -8,27 +10,31 @@ function ContactForm() {
         console.log(data)
     }
   return (
-    <div className='flex  text-[#EBE4D1]'>
-        <div className='flex flex-row'>
+    <div className='flex text-[#EBE4D1]'>
+        <div className='flex flex-row items-end'>
             <form
             onSubmit={handleSubmit(onSubmit)}
-             className='flex flex-col justify-start p-20 bg-[#7B68EE] border-4 my-4 border-[#1b1059] space-y-5 text-black'>
-                <p className='font-heavy text-3xl'>Send me a message!</p>
+             className='flex flex-col justify-start p-20 bg-[rgb(94,69,235)]/60 backdrop-blur-md border-4 border-[#1b1059] space-y-5'>
+                <p className=' whitespace-nowrap font-heavy text-3xl'>Send me a <span className='text-4xl font-bold py-1 border-b-4 border-[#7B68EE]'>message</span> !</p>
                 <div className='flex flex-col justify-start space-y-5'>
+                    <p className='-mb-2 ml-3'>Name or Company * </p>
                     <input
                         name="name"
                         type="text"
-                        placeholder= "Name/Company"
+                        placeholder= "Name"
                         {...register("name", {required:true})}
+                        className='border-4  border-gray-500 text-black p-2 rounded-md focus:outline-none'
                     />
                     {errors.name && <p>Name is required</p>}
+                    <p className='-mb-2 ml-3'>Email * </p>
                     <input
                         name="email"
                         type="email"
-                        placeholder= "Email Address"
+                        placeholder= "Email"
                         {...register("email",{required:true})}
                     />
                     {errors.email && <p>Email is required</p>}
+                    <p className='-mb-2 ml-3'>Message * </p>
                     <textarea
                         name="message"
                         placeholder= "Message"
@@ -40,8 +46,13 @@ function ContactForm() {
                 </div>
 
             </form>
-            <div className="flex flex-col -mt-[50%] -mr-[-50%] p-23  bg-white">
+            <div className="flex flex-col h-[50%] w-[40%] -ml-16 -mb-4 p-8 z-40 text-black rounded-md bg-[#EBE4D1]">
                 <h2>Lets Collaborate</h2>
+                <p>Email<span>Ueharaneal@gmail.com</span></p>
+                <video autoPlay muted loop className='h-[50%] w-[50%]'> 
+                    <source src={squares} type="video/mp4"/>
+                </video>
+                
 
             </div>
         </div>
