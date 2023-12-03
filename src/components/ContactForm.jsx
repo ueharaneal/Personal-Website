41b92d224a780784f04f528b35ffafd05537ad68
Neal Uehara <ueharaneal@gmail.com>
2023-12-02 22:47:24 -0800

    finished contact

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -14,7 +14,7 @@ function ContactForm() {
     console.log(data);
   };
   return (
-    <div className="flex text-[#EBE4D1] mb-16">
+    <div className="flex text-[#e1d5b7] mb-16">
       <div className="flex flex-row items-end">
         <form
           onSubmit={handleSubmit(onSubmit)}
@@ -22,7 +22,7 @@ function ContactForm() {
         >
           <p className=" whitespace-nowrap font-heavy text-3xl">
             Send me a{" "}
-            <span className="text-4xl font-bold py-1 border-b-4 border-[#7B68EE]">
+            <span className="text-4xl font-bold py-1 border-b-4 text-[#d8b867] border-[#7B68EE]">
               message
             </span>{" "}
             !
@@ -32,7 +32,7 @@ function ContactForm() {
             <input
               name="name"
               type="text"
-              placeholder="Name"
+              placeholder="Name/Company"
               {...register("name", { required: true })}
               className="border-4  border-gray-500 text-black p-2  rounded-md focus:outline-none"
             />
@@ -64,23 +64,23 @@ function ContactForm() {
             </button>
           </div>
         </form>
-        <div className="flex flex-col justify-center h-[70%] w-[40%] px-16 -ml-32 -mb-4 p-8 z-40 text-black rounded-md bg-[#e1d5b7] border-8 border-[#7B68EE]">
+        <div className="flex flex-col justify-center h-[70%] w-[40%] px-16 -ml-32 -mb-4 p-8 z-40 text-black rounded-2xl  bg-[#e1d5b7] border-8 border-[#7B68EE]">
           <h2 className="font-bold text-xl font-sans md:text-2xl  p-2 bg-gradient-to-r from-black/20  to-transparent backdrop-blur-sm rounded-xl w-fit">
             Let's <span className="text-[#7B68EE]"> Collaborate</span>!
           </h2>
-          <p className="text-[#2e2370] text-center text-lg md:text-xl z-40 -mb-3">
+          <p className="text-slate-800 text-center text-lg md:text-xl z-40 -mb-3">
             Have an idea?
           </p>
 
           <video autoPlay muted loop className="h-[80%] w-[80%] ml-5">
             <source src={mail} type="video/mp4" />
           </video>
-          <p className="text-[#2e2370] text-center text-lg -mt-2">
+          <p className="text-slate-800 text-center text-lg -mt-2">
             Lets build something amazing!
           </p>
           <div className="flex flex-row">
-            <MdEmail size={12} className="z-60"/>
-            <div className="p-5 z- 20 bg-gradient-to-r from-[#E97451]  to-transparent backdrop-blur-sm rounded-xl">
+            <MdEmail  size={48} className=" text-[#E97451] z-60 mr-2"/>
+            <div className="py-3 z- 20 bg-gradient-to-r from-[#e3a18d] to-transparent backdrop-blur-sm rounded-xl">
                 <span className="px-3">Ueharaneal@gmail.com</span>
             </div>
           </div>
